3ef121d29db54ebf2d619947d32f90d40aa4e52a
pelek1987 <pawel.sioda@gmail.com>
2022-05-15 19:48:57 +0200

    create Search Context and provide to app

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Map} from './Map';
 import {MainLayout} from "./layout/MainLayout";
+import { SearchContext } from 'src/ctx/search.context';
 
 export const App = () => {
+    const [search, setSearch] = useState<string>('')
     return (
         <>
-            <MainLayout>
-                <Map/>
-            </MainLayout>
+            <SearchContext.Provider
+                value={{search, setSearch}}
+            >
+                <MainLayout>
+                    <Map/>
+                </MainLayout>
+            </SearchContext.Provider>
         </>
     );
 }
