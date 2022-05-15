297944e522497cd0d39db2d0f01c5a70dee15f63
pelek1987 <pawel.sioda@gmail.com>
2022-05-15 19:49:33 +0200

    use SearchContext in Header component

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,16 +1,28 @@
-import React from "react";
+import React, {ChangeEvent, ChangeEventHandler, FormEvent, FormEventHandler, useContext, useState} from "react";
 import './Header.css';
 import {Button} from "../../common/Button";
+import {SearchContext} from "../../../ctx/search.context";
 
-export const Header = () => (
-    <header>
-        <h1>
-            <strong>Mega </strong> Ogłoszenia
-        </h1>
-        <Button text="Dodaj ogłoszenie" />
-        <div className="search">
-            <input type="text"/>
-            <Button text="Szukaj" />
-        </div>
-    </header>
-)
+export const Header = () => {
+    const [inputValue, setInputValue] = useState<string>('')
+    const {setSearch} = useContext(SearchContext)
+    const handleInputChange: ChangeEventHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setInputValue(e.target.value);
+    }
+    const setSearchFromLocalState: FormEventHandler = (e: FormEvent) => {
+        e.preventDefault()
+        setSearch(inputValue)
+    }
+    return (
+        <header>
+            <h1>
+                <strong>Mega </strong> Ogłoszenia
+            </h1>
+            <Button text="Dodaj ogłoszenie"/>
+            <form className="search" onSubmit={setSearchFromLocalState}>
+                <input value={inputValue} onChange={handleInputChange} type="text"/>
+                <Button text="Szukaj"/>
+            </form>
+        </header>
+    )
+}
